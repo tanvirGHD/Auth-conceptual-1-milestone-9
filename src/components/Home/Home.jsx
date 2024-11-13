84bacebe4a903d2112ac995821b1ad9664dfc12a
tanvirGHD <155833719+tanvirGHD@users.noreply.github.com>
2024-11-13 17:05:33 +0600

    Login with Github done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,23 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, GithubAuthProvider, signInWithPopup } from "firebase/auth";
 import auth from "../../firebase/firebase.config";
 
 
 const Home = () => {
     const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
+
     const handleGoogleLogin = () =>{
         signInWithPopup(auth, googleProvider)
+        .then(result => console.log(result))
+    }
+    const handleGithubLogin = () =>{
+        signInWithPopup(auth, githubProvider)
         .then(result => console.log(result))
     }
     return (
         <div>
             <button onClick={handleGoogleLogin}>Google Login</button>
+            <button onClick={handleGithubLogin}>Github Login</button>
         </div>
     );
 };
