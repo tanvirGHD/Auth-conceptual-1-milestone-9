1c2744abe2e4f515a5157f5620ef2becd830fd92
tanvirGHD <155833719+tanvirGHD@users.noreply.github.com>
2024-11-14 01:35:39 +0600

    Logout

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -42,11 +42,12 @@ const MainLayout = () => {
             //   // User is signed in, see docs for a list of available properties
             //   // https://firebase.google.com/docs/reference/js/auth.user
             //   const uid = user.uid;
-            //   // ...
+            //   // ..
             // } else {
             //   // User is signed out
-            //   // ...
+            //   // ..
             // }
+
           });
     },[])
 
